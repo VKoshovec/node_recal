b91a775d81414bb0b52b3b2e7fe0eb3c8b10176e
VladimirKoshovec <koshavladmir@gmail.com>
2024-07-23 23:11:57 +0300

    contacts

--- contact.js
+++ contact.js
@@ -1,10 +1,11 @@
 const express = require ('express');
 const router = express.Router();
+const contacts_souse = require('./resourse/contacts.json');
 
-router.use((req,res, next) => {
-    console.log(`It's ${ req.method } req ${ req.url } req`);
-    next();
-});
+// router.use((req,res, next) => {
+//     console.log(`It's ${ req.method } req ${ req.url } req`);
+//     next();
+// });
 
 router.get('/', (req, res)=>{
     res.status(200).json({
@@ -13,9 +14,12 @@ router.get('/', (req, res)=>{
 });
 
 router.get('/all', (req, res)=>{
-    res.status(200).json({
-        '/all respose contacts':'ok'
-    })
+    res.status(200).json(contacts_souse);
+});
+
+router.get('/one', (req, res)=>{
+    const { id } = req.query;
+    res.status(200).json(contacts_souse[id]);
 });
 
 module.exports = router;
