11b7f8294fa9e710091cf758624d8eaa802bba67
VladimirKoshovec <koshavladmir@gmail.com>
2024-07-24 23:11:54 +0300

    ups

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const cors = require('cors');
 const contacts = require('./contact');
 
 app.use(cors());
+// parse application/json
+app.use(express.json());
 
 app.use((req,res, next) => {
     console.log(`It's ${ req.method} req ${ req.url } req`);
--- contact.js
+++ contact.js
@@ -2,11 +2,6 @@ const express = require ('express');
 const router = express.Router();
 const contacts_souse = require('./resourse/contacts.json');
 
-// router.use((req,res, next) => {
-//     console.log(`It's ${ req.method } req ${ req.url } req`);
-//     next();
-// });
-
 router.get('/', (req, res)=>{
     res.status(200).json({
         '/ respose contacts':'ok'
@@ -19,7 +14,22 @@ router.get('/all', (req, res)=>{
 
 router.get('/one', (req, res)=>{
     const { id } = req.query;
-    res.status(200).json(contacts_souse[id]);
+    contacts_souse.length > id?res.status(200).json(contacts_souse[id]):res.status(404).json({ "req": "notfound"});
+});
+
+router.post("/", (req, res)=>{
+   const { name, phone } = req.body;  
+   contacts_souse.push({ name, phone });
+   res.status(201).json(contacts_souse);
+});
+
+router.put('/one', (req, res)=>{
+    const { id } = req.query;
+    const { name, phone } = req.body;
+    contacts_souse[id].name = name;
+    contacts_souse[id].phone = phone;
+
+    res.status(202).json(contacts_souse[id]);
 });
 
 module.exports = router;
